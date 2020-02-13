04748d46824a20e9ac4ba4ca4562380d1eb69d79
MrGio7 <lashagiorgi1233@gmail.com>
2020-02-13 13:32:53 +0400

    imported local data

--- src/snackBarApp/Main.java
+++ src/snackBarApp/Main.java
@@ -2,6 +2,19 @@ package snackBarApp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("*** SnackBar ***");
+        System.out.println("*** SnackBar App ***");
+
+        Customer c1 = new Customer("Jane", 45.25);
+        Customer c2 = new Customer("Bob", 33.14);
+
+        VendingMachine v1 = new VendingMachine("Food");
+        VendingMachine v2 = new VendingMachine("Drink");
+        VendingMachine v3 = new VendingMachine("Office");
+
+        Snack s1 = new Snack("Chips", 36, 1.75, 1);
+        Snack s2 = new Snack("Chocolate", 36, 1.00, 1);
+        Snack s3 = new Snack("Pretzel", 30, 2.00, 1);
+        Snack s4 = new Snack("Soda", 24, 2.50, 2);
+        Snack s5 = new Snack("Water", 20, 2.75, 2);
     }
 }
